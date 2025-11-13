2d65c6dc8b9a9125e78a8c0f7dbb9ec18282b054
Baran <baran@82d.wls.metu.edu.tr>
2025-11-13 13:11:22 +0300

    Functions completed.

--- task.py
+++ task.py
@@ -19,7 +19,7 @@ delete_parser.add_argument("id", type=int, help="Id of task")
 mark_in_progress_parser = subparsers.add_parser("mark-in-progress")
 mark_in_progress_parser.add_argument("id", type=int)
 
-mark_done_parser = subparsers.add_parser("mark-done-progress")
+mark_done_parser = subparsers.add_parser("mark-done")
 mark_done_parser.add_argument("id", type=int)
 
 list_parser = subparsers.add_parser("list")
@@ -35,4 +35,13 @@ tm = TaskManager()
 
 if args.command == "add":
     tm.add_task(args.description)
-    
+elif args.command == "update":
+    tm.update_task(args.id, args.description)
+elif args.command == "delete":
+    tm.delete_task(args.id)
+elif args.command == "mark-in-progress":
+    tm.change_status_task(args.id, "in-progress")
+elif args.command == "mark-done":
+    tm.change_status_task(args.id, "done")
+elif args.command == "list":
+    tm.list_task(args.status)
--- task_functions.py
+++ task_functions.py
@@ -23,6 +23,12 @@ class TaskManager:
             return 1
         return max(task["id"] for task in self.tasks) + 1
     
+    def task_finder(self, id):
+        for task in self.tasks:
+            if task["id"] == id:
+                return task
+        return {}
+    
     def add_task(self, description):
         now = datetime.now().isoformat()
         task = {"id": self.next_id(), "description": description, "status": "todo", "createdAt":now, "updatedAt":now}
@@ -30,3 +36,47 @@ class TaskManager:
         self.save_tasks()
         print(f"Task added successfully (ID: {task['id']})")
     
+    def update_task(self, id, new_description):
+        task = self.task_finder(id)
+        if task == {}:
+            print(f"Task with id {id} not found.")
+            return
+        task["description"] = new_description
+        task["updatedAt"] = datetime.now().isoformat()
+        self.save_tasks()
+        print(f"Task {id} updated successfully.")
+
+    def delete_task(self, id):
+        task = self.task_finder(id)
+        if task == {}:
+            print(f"Task with id {id} not found.")
+            return
+        self.tasks.remove(task)
+        self.save_tasks()
+        print(f"Task {id} deleted successfully.")
+
+    def change_status_task(self, id, new_status):
+        task = self.task_finder(id)
+        if task == {}:
+            print(f"Task with id {id} not found.")
+            return
+        if(task["status"] == new_status):
+            print(f"Task {id} is already marked as {new_status}.")
+            return
+        task["status"] = new_status
+        task["updatedAt"] = datetime.now().isoformat()
+        self.save_tasks()
+        print(f"Task {id} has been marked as {new_status}.")
+
+    def list_task(self, status):
+        if status:
+            tasks_to_show = [task for task in self.tasks if task["status"] == status]
+        else:
+            tasks_to_show = self.tasks
+
+        if not tasks_to_show:
+            print("No tasks found.")
+            return
+        
+        for task in tasks_to_show:
+            print(f"[{task['id']}] {task['description']} - {task['status']}")
